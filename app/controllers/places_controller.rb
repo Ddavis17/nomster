5530043cdc9d593ad09d03b06676e88e2e3ccacf
dustin davis <dustin.davis2244@gmail.com>
2019-06-11 13:22:51 +0000

    added boxes in places form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,11 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.all(page: params[:index], per_page: 10)
+
   end
+
+  def new
+     @place = Place.new
+  end
+
 end
