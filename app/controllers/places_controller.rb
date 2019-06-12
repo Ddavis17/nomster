751ab7db77cda64e72fbd569bef6f343ef12af8c
dustin davis <dustin.davis2244@gmail.com>
2019-06-12 14:19:59 +0000

    finished the edit page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,47 +1,57 @@
 class PlacesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create]
 
-
+  # shows all the places in the db(w/ the exception of our limit of 10)
   def index
+
     @places = Place.all
+
   end
 
 
 
-
+# page to add a place
   def new
+
      @place = Place.new
+
   end
 
 
+# button to create place and redirect to root page
 
+  def create
 
-   def create
     current_user.places.create(place_params)
     redirect_to root_path
+
   end
 
-
-def show 
+# the place to see indiv. places
+  def show 
   @place = Place.find(params[:id])
   end
   
-def edit
-  <%= @place = Place.find(parmas[:id]) %>
+# page to edit an exsiting page
+ def edit
+   @place = Place.find(parmas[:id]) 
   
-end
+ end
+
+# when you select the update button, this executes
+ def update
+  @place = Place.find(parmas[:id])
+  @place.update_attributes(place_params)
+  redirect_to root_path
+ end
+
+private
 
 
 
 
-
-  private
-
-
-
-
-def place_params
+  place_params
     params.require(:place).permit(:name, :description, :address)
   end
 
-end
+  end
