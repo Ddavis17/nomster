a3234665041e7dbd1215a38446813c21d970a331
dustin davis <dustin.davis2244@gmail.com>
2019-06-15 12:50:13 +0000

    bug fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,90 +1,77 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   # shows all the places in the db(w/ the exception of our limit of 10)
   def index
-
     @places = Place.all
     Place.paginate(page: params[:page], per_page: 10)
-
   end
-
-
 
 # page to add a place
   def new
-
-     @place = Place.new
-
+    @place = Place.new
   end
-
 
 # button to create place and redirect to root page
 
   def create
+    @place = current_user.places.create(place_params)
 
-   @place = current_user.places.create(place_params)
-  if @place.valid? 
+    if @place.valid? 
       redirect_to root_path
-  else
+    else
       render :new, status: :unprocessable_entity
-  end
+    end
   end
 
 # the place to see indiv. places
   def show 
-  @place = Place.find(params[:id])
-  @comment = Comment.new
+    @place = Place.find(params[:id])
+    @comment = Comment.new
   end
   
 # page to edit an exsiting page
- def edit
-   @place = Place.find(params[:id]) 
+  def edit
+    @place = Place.find(params[:id]) 
 
-
-   if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
-  
- end
 
 # when you select the update button, this executes
- def update
+  def update
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
+
+    @place.update_attributes(place_params)
+
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
+  def destroy 
+    @place = Place.find(params[:id])
 
-  @place.update_attributes(place_params)
-  if @place.valid?
-    redirect_to root_path
- else
-    render :edit, status: :unprocessable_entity
-  end
-
-
-
- def destroy 
-  @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
 
+    @place.destroy
+    redirect_to root_path
+  end
 
-  @place.destroy
-  redirect_to root_path
- end
-
-private
-
-
-
+  private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
+end
 
 
 
