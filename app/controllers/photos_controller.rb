099174751f4d0e12f557c9cba49040e25b3376a4
dustin davis <dustin.davis2244@gmail.com>
2019-07-15 00:04:29 +0000

    fixing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,13 +2,13 @@ class PhotosController < ApplicationController
   before_action :authenticate_user!
    def create
       @place = Place.find(params[:place_id])
-      @place.photos.create(photos_params.merge(user: current_user))
+      @place.photos.create(photo_params.merge(user: current_user))
       redirect_to place_path(@place)
   end
 
   private 
 
   def photo_params
-    params.require(:place_id, :picture)
+    params.require(:place_id)
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIA5V6BO5BKXCYUWEGW"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["nomster-bucket-000001"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
