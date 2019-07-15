5cb73ba82c3f806e6b94562b01b88b678a1f68d7
dustin davis <dustin.davis2244@gmail.com>
2019-07-15 01:26:25 +0000

    fixing bug

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,11 +2,11 @@ class PhotosController < ApplicationController
   before_action :authenticate_user!
    def create
       @place = Place.find(params[:place_id])
-      @place.photos.create(photo_params.merge(user: current_user))
+      @place.photos.create(photo.params.merge(user: current_user))
       redirect_to place_path(@place)
   end
 
-  private 
+  # private
 
   def photo_params
     params.require(:place_id)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :user 
   belongs_to :place
+  after_create :send_comment_email
+
 end
 class Photos < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
