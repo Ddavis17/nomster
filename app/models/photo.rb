42dfc8ddeed4f8e1a98ca0e496b6d187393e88c9
dustin davis <dustin.davis2244@gmail.com>
2019-07-22 14:25:35 +0000

    bug fixes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,5 @@ class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
   # after_create :send_comment_email
-
-end
-class Photos < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-    resources :places do
+
+  resources :places do
     resources :photos, only: :create
     resources :comments, only: :create
   end
  
- resources :users, only: :show
+  resources :users, only: :show
 end
